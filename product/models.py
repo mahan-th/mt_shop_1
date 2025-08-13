d92cbbd69ce642a45c1418377f43887de1a44463
mahan <mahan.tahmasbi85@gmail.com>
2025-08-13 03:47:43 -0500

    I'm triying to implenment the database model in the best way

--- product/models.py
+++ product/models.py
@@ -44,7 +44,7 @@ class Warranti(models.Model):
 class Review(models.Model):
     product = models.ForeignKey(Product ,on_delete=models.CASCADE ,related_name='reviews')
 
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     comment = models.TextField(blank=True)
 
@@ -60,6 +60,7 @@ class Review(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(unique=True ,blank=True, allow_unicode=True)
+    parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True, related_name="children")
 
     def __str__(self):
         return f'Category |{self.title}'
